59d1e7e44a7077244fc780060ad89d1172e201f0
mariia-ty <tymoshenkom@icloud.com>
2024-12-08 13:27:45 -0500

    added box

--- students/mariia.js
+++ students/mariia.js
@@ -1,8 +1,16 @@
 const container = document.getElementById("output-container");
 
+const boxm = document.createElement("div");
+boxm.classList.add("box-mariia");
+// boxm.style.width = "450px";
+boxm.style.height = "350px";
+boxm.style.backgroundColor = "lightpink";
+boxm.style.border = "7px dotted lightgreen";
+container.append(boxm);
+
 const header = document.createElement("h4");
 header.textContent = "Mariia's Corner";
-container.appendChild(header);
+boxm.appendChild(header);
 
 const img = document.createElement("img");
 img.src =
@@ -10,5 +18,7 @@ img.src =
 img.style.width = "450px"; 
 img.style.height = "250px";
 img.alt = "chipmunck eating corn";
-container.appendChild(img);
+boxm.appendChild(img);
+
+
 
