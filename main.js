117bfe759e7866f893e2e935f67fab8190651cce
mariia-ty <tymoshenkom@icloud.com>
2024-12-08 12:45:46 -0500

    added

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ import "./students/example.js";
 import "./students/Nazar.js";
 import "./students/student1.js";
 import "./students/student2.js";
+import "./students/mariia.js";
---
+++ students/mariia.js
@@ -0,0 +1,7 @@
+const container = document.getElementById("output-container");
+
+const header = document.createElement("h4");
+header.textContent = "Mariia's Corner";
+container.appendChild(header);
+
+
