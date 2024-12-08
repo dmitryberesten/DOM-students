c1810659309615ed47012703df775e912fa273bd
papa2008 <112895777+papa2008@users.noreply.github.com>
2024-12-08 19:18:39 +0100

    gg

--- students/Vladyslav.js
+++ students/Vladyslav.js
@@ -1,4 +1,6 @@
-const container = document.getElementById("container");
+const container = document.getElementById("output-container");
 const header = document.createElement("h4");
+button.textContent = "vladyslav buttom";
+container.appendChild(button);
 header.textContent = "vladyslav buttom";
 container.appendChild(header);
