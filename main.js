ffdca44b2da7906736f736336323d973ebdd91b9
NazarKutoviy <147269741+NazarKutoviy@users.noreply.github.com>
2024-12-08 19:43:55 +0200

    hack atack

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 import "./students/example.js";
-
+import "./students/Nazar.js";
 import "./students/student1.js";
 import "./students/student2.js";
---
+++ students/Nazar.js
@@ -0,0 +1,4 @@
+const container = document.getElementById("output-container");
+const button = document.createElement("button");
+button.textContent = "скачати торент";
+container.appendChild(button);
