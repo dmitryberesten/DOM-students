6bbb89a0bf7993642e0f78048280e1f72e678086
papa2008 <112895777+papa2008@users.noreply.github.com>
2024-12-08 19:22:20 +0100

    gg

--- students/Vladyslav.js
+++ students/Vladyslav.js
@@ -4,3 +4,17 @@ button.textContent = "vladyslav buttom";
 container.appendChild(button);
 header.textContent = "vladyslav buttom";
 container.appendChild(header);
+
+const VladyslavHeading = document.createElement('h2');
+VladyslavHeading.textContent = 'Це заголовок від Vladyslav'; 
+VladyslavHeading.classList.add('main-heading'); 
+
+const container = document.getElementById('container'); 
+container.append(VladyslavHeading);
+
+const VladyslavBox = document.createElement('div');
+VladyslavBox.textContent = 'Це коробка від Vladyslav';
+VladyslavBox.classList.add('box'); 
+VladyslavBox.addEventListener('click', () => {
+  VladyslavBox.classList.toggle('highlight'); 
+});
